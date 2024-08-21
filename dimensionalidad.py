6e157b688fba47982502341442eed78bf98ff3dc
Joan Roa -joanroamora- on gitHub <joanroamora@gmail.com>
2024-08-21 18:46:38 -0500

    Agregando dimensioanlidad al proyecto

--- dimensionalidad.py
+++ dimensionalidad.py
@@ -3,6 +3,9 @@ from sklearn.decomposition import PCA
 
 # Función para aplicar PCA al dataset
 def apply_pca(df, n_components=2):
+    # Mantener los nombres de los países
+    countries = df['Country']
+
     # Seleccionar solo las columnas numéricas
     numeric_cols = df.select_dtypes(include=[float, int])
 
@@ -10,8 +13,10 @@ def apply_pca(df, n_components=2):
     pca = PCA(n_components=n_components)
     principal_components = pca.fit_transform(numeric_cols)
 
-    # Crear un nuevo DataFrame con las componentes principales
+    # Crear un nuevo DataFrame con las componentes pclear
+    # rincipales y los nombres de los países
     pca_df = pd.DataFrame(data=principal_components, columns=[f'PC{i+1}' for i in range(n_components)])
+    pca_df.insert(0, 'Country', countries)  # Insertar los nombres de los países al principio
     
     return pca_df, pca.explained_variance_ratio_
 
